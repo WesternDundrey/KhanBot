f850946f1784f54146da14d5686ae5809778721e
Westerndundrey <hijackedpuffin@gmail.com>
2024-12-07 16:45:42 +0800

    some more light touches.

--- dashboard/start_dashboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate the dashboard env
-source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate dashboard
-
-# Start Streamlit dashboard on port 8501 by default
-cd ../dashboard
-make run
